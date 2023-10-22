0952398d8f1e9f49641d7ad7813eca6f5af07099
@ivan-aksenov <rivenmain098@gmail.com>
2023-10-22 16:38:41 +0800

    first commit:

---
+++ my_thesis/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+from .models import TodoItem
+
+# Create your views here.
+
+def home(request):
+    return render(request, "home.html")
+
+def todos(request):
+    data = TodoItem.objects.all()
+    return render(request,"todos.html", {"todos": data})
