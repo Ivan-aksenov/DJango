6073a3bf27cff454ee72fa15df1693697644092a
keannu-delim <keannu.cedrick.delim24@gmail.com>
2023-10-25 21:22:43 +0800

    sixth commit

--- my_thesis/urls.py
+++ my_thesis/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path("", views.Home, name="Home"),
     path("todos/", views.todos, name="Todos"),
     path("SignIn/", views.SignIn, name="SignIn"),
-    path("SignUp/", views.SignUp, name="SignUp")
+    path("SignUp/", views.SignUp, name="SignUp"),
+    path("Features/", views.Features, name="Features"),
+    path("Insights/", views.Insights, name="Insights"),
+    path("AboutUs/", views.AboutUs, name="AboutUs"),
+    path("Contact/", views.Contact, name="Contact")
 ]
--- my_thesis/views.py
+++ my_thesis/views.py
@@ -3,15 +3,27 @@ from .models import TodoItem
 
 # Create your views here.
 
-def Home(request):
-    return render(request, "Home.html")
-
 def todos(request):
     data = TodoItem.objects.all()
     return render(request,"todos.html", {"todos": data})
 
+def Home(request):
+    return render(request, "my_thesis/Home.html")
+
 def SignIn(request):
-    return render(request,"SignIn.html")
+    return render(request,"my_thesis/SignIn.html")
 
 def SignUp(request):
-    return render(request,"SignUp.html")
+    return render(request,"my_thesis/SignUp.html")
+
+def Features(request):
+    return render(request, "my_thesis/Features.html")
+
+def Insights(request):
+    return render(request, "my_thesis/Insights.html")
+
+def AboutUs(request):
+    return render(request, "my_thesis/AboutUs.html")
+
+def Contact(request):
+    return render(request, "my_thesis/Contact.html")
